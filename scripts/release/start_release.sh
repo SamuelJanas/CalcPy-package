8709b9c4f50512ad2a5cb1c8b98f2e9285780bc8
Samuel Janas <sneakyinsect@gmail.com>
2023-11-13 16:20:40 +0100

    script with placeholder might work?

--- scripts/release/start_release.sh
+++ scripts/release/start_release.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Check if an argument was provided
 if [ $# -eq 0 ]; then
     echo "No version type specified. Usage: ./start_release.sh [major/minor/patch]"
@@ -13,15 +15,25 @@ if [[ "$VERSION_TYPE" != "major" && "$VERSION_TYPE" != "minor" && "$VERSION_TYPE
     exit 1
 fi
 
+# Placeholder for the new version
+PLACEHOLDER_VERSION="0.0.0"
+
+# Starting a new Git Flow release with a placeholder version
+git flow release start $PLACEHOLDER_VERSION
+
 # Updating the version using bumpver
 bumpver update --$VERSION_TYPE
 
 # Extracting the new version number
-# Using grep with a regex to find the version number
 NEW_VERSION=$(bumpver show | grep -oP 'Current Version: \K.*')
 
-# Starting a new Git Flow release
-git flow release start $NEW_VERSION
+# Rename the Git Flow release branch to the correct version
+# Assuming default naming convention for Git Flow release branches
+git branch -m release/$PLACEHOLDER_VERSION release/$NEW_VERSION
+
+# Update the version files
+git add .
+git commit -m "Bump version to $NEW_VERSION"
 
 # Reminder message
 echo "Release $NEW_VERSION started. Make any last-minute changes now."
